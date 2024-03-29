816fd1c817fdea846aa57c4fa2b0d6386ba97aec
christian-bayata <franksagie1@gmail.com>
2024-03-29 16:27:04 +0100

    Initial setup with auth interceptor

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+import { AuthMsLogger } from './common/logger.interceptor';
+
+async function bootstrap() {
+  const logger = new Logger('AuthMicroservice');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL],
+        queue: 'auth_queue',
+        queueOptions: {
+          durable: true,
+        },
+      },
+    },
+  );
+
+  app.useGlobalInterceptors(new AuthMsLogger());
+  await app
+    .listen()
+    .finally(() =>
+      logger.log(`Auth Microservice: EventBus:${process.env.RABBITMQ_URL}`),
+    );
+}
+bootstrap();
